26e942cade3cc7a6d35d83e6c7b5d71092dea375
AbdelKarimBenzema <150791486+AbdelKarimBenzema@users.noreply.github.com>
2023-11-14 22:28:38 +0100

    Update Juegos.sql

--- Juegos.sql
+++ Juegos.sql
@@ -1,16 +1,15 @@
-CREATE DATABASE IF NOT EXISTS PEPS;
-USE PEPS;
-CREATE TABLE juegos(
+CREATE DATABASE IF NOT EXISTS DREAM;
+USE DREAM;
+CREATE TABLE usuarios(
     id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(255) NOT NULL,
-    descripcion VARCHAR(255) NOT NULL,
-    precio DECIMAL(9,2) NOT NULL,
-	foto VARCHAR(255)
+    apellido1 VARCHAR(255) NOT NULL,
+    apellido2 VARCHAR(255) NOT NULL,
+    fechanacimiento DATE NOT NULL,
 );
-CREATE TABLE usuarios(
+CREATE TABLE SECTORES(
 	usuario VARCHAR(100) NOT NULL PRIMARY KEY,
     clave VARCHAR(255) NOT NULL,
-    perfil VARCHAR(100) NOT NULL,
-    fechaUltimoAcceso DATE
+  
 );
-INSERT INTO `usuarios` (`usuario`, `clave`, `perfil`, `fechaUltimoAcceso`) VALUES ('root', '1234', 'admin', '2022-03-01');
+INSERT INTO `usuarios` (`nombre`, `apellido1`, `apellido2`, `fechanacimiento`) VALUES ('root', 'root', 'root', '2022-03-01');
