5c17a53d5064fe81dc7f0cf800cab79c09622347
David <abdelkarimb776@gmail.com>
2023-11-19 19:56:25 +0100

    mensaje

--- Juegos.sql
+++ Juegos.sql
@@ -1,17 +1,17 @@
 CREATE DATABASE IF NOT EXISTS DREAM;
 USE DREAM;
 CREATE TABLE usuarios(
-    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(255) NOT NULL,
-    apellido1 VARCHAR(255) NOT NULL,
-    apellido2 VARCHAR(255) NOT NULL,
-    fechanacimiento DATE NOT NULL,
-);
+  id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(255) NOT NULL,
+  apellido1 VARCHAR(255) NOT NULL,
+  apellido2 VARCHAR(255) NOT NULL,
+  fechanacimiento DATE NOT NULL
+) ;
 CREATE TABLE SECTORES(
 	usuario VARCHAR(100) NOT NULL PRIMARY KEY,
     clave VARCHAR(255) NOT NULL,
     perfil VARCHAR(100) NOT NULL,
     fechaUltimoAcceso DATE
-);
+) ;
 
-INSERT INTO `usuarios` (`nombre`, `apellido1`, `apellido2`, `fechanacimiento`) VALUES ('root', 'root', 'root', '2022-03-01');
+INSERT INTO usuarios (nombre, apellido1, apellido2, fechanacimiento) VALUES ('root', 'root', 'root', '2022-03-01');
