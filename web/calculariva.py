0d3f42340156cad6fb119c34f9464a3229b9d43d
AbdelKarimBenzema <150791486+AbdelKarimBenzema@users.noreply.github.com>
2024-03-25 11:51:15 +0100

    Delete web directory

--- web/calculariva.py
+++
@@ -1,4 +0,0 @@
-def calculo(importe):
-    iva = 0.21
-    return importe * iva
-print(calculo(10))
--- web/calcularivaTest.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from calculariva import calculo
-
-class tests_calculariva(unittest.TestCase):
-    def test_calculo(self):
-        self.assertEqual(calculo(100),21)
-if __name__ == '__main__':
-    unittest.main()
